4810a4623cd56e794abdfd971183f75edd4f0d6b
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-03-19 14:51:36 -0400

    Add structure to single blog post

---
+++ single.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="container single">
+	<?php 
+		if(have_posts()){
+			while(have_posts()){
+				the_post();?>
+				<h2><?php the_title(); ?></h2>
+				<p class="p"><?php echo "Published: " . get_the_date(); ?></p>
+				<p class="p l"><?php echo "Article written by: " . get_the_author(); 
+				?></p>
+				<?php the_content(); ?>
+				
+			<?php } // end while loop
+		}// end if statement
+	?>
+</div>
+<?php get_footer(); ?>
