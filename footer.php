658e6ef00b67825a23462266838617022aa1c588
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2019-03-18 19:23:31 -0500

    Add menu location to footer

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="twelve columns">
+        <?php wp_nav_menu(array(
+          'theme_location' => 'footer-menu',
+          'container-class' => 'menu-footer'
+        ));
+        ?>
+      </div>
+    </div>
+  </div>
+</footer>
 
   <?php wp_footer(); ?>
   </body>
