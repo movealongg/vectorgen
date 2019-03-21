862bdadb186fe66b4839ed52600d19b180b32593
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2019-03-20 20:10:36 -0500

    Create contact text block widget

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-//Add custom menus
+//Add Custom Menus
 function custom_menus(){
   register_nav_menus(array(
     'header-menu' => __('Header Menu'),
@@ -12,7 +12,19 @@ add_action('init', 'custom_menus');
 // BLOG PICTURES
 	add_theme_support('post-thumbnails');
 
-//Add widget areas
+//Add Widget Areas
+function blank_widgets_init(){
+  //Contact Text Block Widget
+  register_sidebar( array(
+    'name'          => ('Contact Text Block'),
+    'id'            => 'contact-us',
+    'description'   => 'Widget area for Contact Us section',
+    'before_widget' => '<div class="contact-us">',
+    'after_widget'  => '</div>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>'
+  ));
+}
 
 
 
