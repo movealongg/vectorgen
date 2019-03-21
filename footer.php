ccaa2f9e6e32d0e4a3dfa1a7db3287ffbe25b088
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2019-03-20 19:17:45 -0500

    Add copyright to footer

--- footer.php
+++ footer.php
@@ -5,8 +5,11 @@
         <?php wp_nav_menu(array(
           'theme_location' => 'footer-menu',
           'container-class' => 'menu-footer'
-        ));
-        ?>
+        ));?>
+      </div>
+
+      <div class="four columns">
+        <p>&copy; <?php echo date("Y"); echo " "; echo bloginfo('name'); ?></p>
       </div>
     </div>
   </div>
