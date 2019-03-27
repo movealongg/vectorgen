657660c705de06894e730abfa75d25de0a0e7435
Aliyah Northington <alierin@aliyahs-mbp.attlocal.net>
2019-03-26 20:18:50 -0500

    Redo bg color on home section

--- page-home.php
+++ page-home.php
@@ -6,13 +6,15 @@
 			<?php dynamic_sidebar('hero-image'); ?>
 </div>
 
-<div class="container">
-  <!--Contact Text Block Section-->
-  <div class="row">
-    <div class="eleven columns">
-      <?php dynamic_sidebar('home-block'); ?>
-    </div>
-  </div>
+<div class="home-block-color">
+	<div class="container">
+	  <!--Contact Text Block Section-->
+	  <div class="row">
+	    <div class="eleven columns">
+	      <?php dynamic_sidebar('home-block'); ?>
+	    </div>
+	  </div>
+	</div>
 </div>
 
 
