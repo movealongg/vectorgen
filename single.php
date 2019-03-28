64e711c115d95b574c6b303fe319fa77a33f317f
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-03-27 23:37:00 -0400

    add styles

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
 			if(have_posts()){
 				while(have_posts()){
 					the_post();?>
-					<h2><?php the_title(); ?></h2>
+					<h2 class="blog-post-title"><?php the_title(); ?></h2>
 					<p class="p"><?php echo "Published: " . get_the_date(); ?></p>
 					<p class="p l"><?php echo "Author: " . get_the_author(); 
 					?></p>
