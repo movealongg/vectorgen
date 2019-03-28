efbfd7a734e0fd49df59520ce29c30642aa8eb21
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-03-27 22:31:39 -0400

    adjust code

--- single.php
+++ single.php
@@ -1,20 +1,21 @@
 <?php get_header(); ?>
 
-<div class="container single">
 	<?php the_post_thumbnail('full-size'); ?>
 
-		<?php 
-			if(have_posts()){
-				while(have_posts()){
-					the_post();?>
-					<h2><?php the_title(); ?></h2>
-					<p class="p"><?php echo "Published: " . get_the_date(); ?></p>
-					<p class="p l"><?php echo "Author: " . get_the_author(); 
-					?></p>
-					<?php the_content(); ?>
-					
-				<?php } // end while loop
-			}// end if statement
-		?>
-</div>
+		<div class="container single">
+
+				<?php 
+					if(have_posts()){
+						while(have_posts()){
+							the_post();?>
+							<h2><?php the_title(); ?></h2>
+							<p class="p"><?php echo "Published: " . get_the_date(); ?></p>
+							<p class="p l"><?php echo "Author: " . get_the_author(); 
+							?></p>
+							<?php the_content(); ?>
+							
+						<?php } // end while loop
+					}// end if statement
+				?>
+		</div>
 <?php get_footer(); ?>
