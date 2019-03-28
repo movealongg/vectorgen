177c299a40dff6a71d461904421b39fabccccfe8
Robert Cole <robertcole@Roberts-MacBook-Pro.local>
2019-03-28 16:27:30 -0400

    Add css to style our plan widgets

--- page-ourplan.php
+++ page-ourplan.php
@@ -2,30 +2,29 @@
 
 <?php get_header(); ?>
 
-<div class="container">
-    <h5 class="our_plan">Our Plan</h5>
+<div class="ourplan_container">
 
+    <div class="container">
+        <h5 class="our_plan">Our Plan</h5>
 
-<!--Stage 1 Section -->
-    <div class="row stage1">
-        <div class="six columns">
-            <div class="our-plan-stage1-text">
-                <?php dynamic_sidebar('stage1-text'); ?>
+    <!--Stage 1 Section -->
+        <div class="row stage1">
+            <div class="six columns">
+                <div class="our-plan-stage1-text">
+                    <?php dynamic_sidebar('stage1-text'); ?>
+                </div>
             </div>
-        </div>
-        <div class="six columns">
-            <div class="our-plan-stage1-image">
-                <?php dynamic_sidebar('stage1-image'); ?>
+            <div class="six columns">
+                <div class="our-plan-stage1-image">
+                    <?php dynamic_sidebar('stage1-image'); ?>
+                </div>
             </div>
-        </div>
-    </div>  
-
-
+        </div> 
+    </div> 
 
 
 
 </div>
 
 
-
-// <?php get_footer(); ?>
+<?php get_footer(); ?>
