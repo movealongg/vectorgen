7269735eccffe990668abd33c1fa184cc81d8768
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-25 18:20:46 -0400

    comments

--- index.php
+++ index.php
@@ -14,11 +14,15 @@
 					if(have_posts()){
 						while (have_posts()){
 							the_post(); ?>
+							<!-- Blog Image SECTION --> 
 							<div class="one-third column posts">
 
 								<div class="animated fadeIn duration2"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a></div>
+								<!-- Blog Title SECTION --> 
 								<h3><a href="<?php the_permalink(); ?>" class = "blog-title animated fadeIn duration2" ><?php the_title();?></a></h3>
+								<!-- Published SECTION --> 
 								<p class="p hide"><?php echo "Published: " . get_the_date(); ?></p>
+								<!-- Author SECTION --> 
 								<p class="p l hide"><?php echo "Author: " . get_the_author(); ?></p>
 							</div>
 
