d570dea387340d879368b9b59934782fcc995fb3
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-25 13:49:22 -0400

    top class for mobile nav

--- index.php
+++ index.php
@@ -1,32 +1,33 @@
 <?php /*Template Name: Blog Page Template*/ ?>
 
 <?php get_header(); ?>
+	<div class="top">
+		<div class="container blog">
+			<div class="row">
+	        	<?php dynamic_sidebar('blog-title'); ?>
+	      	</div>
 
-	<div class="container blog">
-		<div class="row">
-        	<?php dynamic_sidebar('blog-title'); ?>
-      	</div>
+				<div class="row blog-row">
 
-			<div class="row blog-row">
+				<?php
 
-			<?php
+					if(have_posts()){
+						while (have_posts()){
+							the_post(); ?>
+							<div class="one-third column posts">
 
-				if(have_posts()){
-					while (have_posts()){
-						the_post(); ?>
-						<div class="one-third column posts">
+								<div class="animated fadeIn duration2"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a></div>
+								<h3><a href="<?php the_permalink(); ?>" class = "blog-title animated fadeIn duration2" ><?php the_title();?></a></h3>
+								<p class="p hide"><?php echo "Published: " . get_the_date(); ?></p>
+								<p class="p l hide"><?php echo "Author: " . get_the_author(); ?></p>
+							</div>
 
-							<div class="animated fadeIn duration2"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('medium'); ?></a></div>
-							<h3><a href="<?php the_permalink(); ?>" class = "blog-title animated fadeIn duration2" ><?php the_title();?></a></h3>
-							<p class="p hide"><?php echo "Published: " . get_the_date(); ?></p>
-							<p class="p l hide"><?php echo "Author: " . get_the_author(); ?></p>
-						</div>
-
-			     <?php	} //ends while loop
-				} // end if statement
-			?>
-			</div>
-	</div>
+				     <?php	} //ends while loop
+					} // end if statement
+				?>
+				</div>
+		</div>
+	</div>	
 
 
 
--- single.php
+++ single.php
@@ -1,22 +1,24 @@
 <?php get_header(); ?>
 
-<div class="container single">
-	<div class="container-feature">
-		<div class="hero-image-blog"><?php the_post_thumbnail('large'); ?></div>
-	</div>	
+	<div class="top">
+		<div class="container single">
+			<div class="container-feature">
+				<div class="hero-image-blog"><?php the_post_thumbnail('large'); ?></div>
+			</div>	
 
-		<?php 
-			if(have_posts()){
-				while(have_posts()){
-					the_post();?>
-					<h2 class="blog-post-title"><?php the_title(); ?></h2>
-					<p class="p"><?php echo "Published: " . get_the_date(); ?></p>
-					<p class="p l"><?php echo "Author: " . get_the_author(); 
-					?></p>
-					<div class="blog-post-content" ><?php the_content(); ?></div>
-					
-				<?php } // end while loop
-			}// end if statement
-		?>
-</div>
+				<?php 
+					if(have_posts()){
+						while(have_posts()){
+							the_post();?>
+							<h2 class="blog-post-title"><?php the_title(); ?></h2>
+							<p class="p"><?php echo "Published: " . get_the_date(); ?></p>
+							<p class="p l"><?php echo "Author: " . get_the_author(); 
+							?></p>
+							<div class="blog-post-content" ><?php the_content(); ?></div>
+							
+						<?php } // end while loop
+					}// end if statement
+				?>
+		</div>
+	</div>	
 <?php get_footer(); ?>
