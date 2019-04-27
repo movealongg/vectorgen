019c0e6b9667ad86d49ad9e6cf9fd1f54c698155
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2019-04-27 12:28:02 -0400

    add to page.php

--- page.php
+++ page.php
@@ -1,4 +1,20 @@
 <?php get_header(); ?>
- 
+
+ <div class="top">  <!--For Mobile-->
+ 	<div class="container"> <!--Container-->
+ 		<!--Section 1-->
+		<div class="row"> <!--Row 1-->
+			<div class="twelve columns"> <!--Column 1-->
+
+			</div>
+		</div>	
+		<!--Section 2-->
+		<div class="row"> <!--Row 1-->
+			<div class="twelve columns"> <!--Column 1-->
+
+			</div>
+		</div>	
+	</div>				
+ </div>	
 
 <?php get_footer(); ?>
